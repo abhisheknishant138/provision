452cc0ba1cc0b5d02b291a06ea507a45d11e05aa
prakriti07 <57622944+prakriti07@users.noreply.github.com>
2020-08-18 13:12:04 +0530

    Update 00_aone_test.go

--- cli/00_aone_test.go
+++ cli/00_aone_test.go
@@ -11,11 +11,8 @@ func TestLoadIncrementer(t *testing.T) {
 	cc:= cliTest(false, false,
 		"plugin_providers", "upload", "incrementer", "from", path.Join("../bin", runtime.GOOS, runtime.GOARCH, "incrementer")).run(t)
 	fmt.Println("passsed 1")
-	fmt.Println (cc)
 	tt:= cliTest(false, false, "plugin_providers", "list").run(t)
 	fmt.Println("passsed 2")
-	fmt.Println (tt)
 	rr:= cliTest(false, false, "plugin_providers", "show", "incrementer").run(t)
 	fmt.Println("passsed 3")
-	fmt.Println (rr)
 }
